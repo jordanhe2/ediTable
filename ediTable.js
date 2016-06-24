073eb1d1ac5fd924d673fa9e285caa57f992f79d
jordanhe2 <jordanhe2@gmail.com>
2016-06-23 21:18:28 -0500

    Progress.

--- ediTable.js
+++ ediTable.js
@@ -14,7 +14,14 @@ var EdiTable = function (table, options) {
             if (typeof optStart == "undefined") optStart = 0;
             if (typeof optEnd == "undefined") optEnd = this.cells.length - 1;
 
-            // ...
+            // Loop this.cells and select/deselect
+            for (var i = 0; i < this.cells.length; i++) {
+                if (i >= optStart && i <= optEnd){
+                    this.cells[i].select();
+                } else {
+                    // deselect
+                }
+            }
         },
         insert : function(optIndex, optValue){
             // Normalize parameters
@@ -22,8 +29,11 @@ var EdiTable = function (table, options) {
             if (typeof optValue == "undefined") optValue = "";
 
             // ...
+        },
+        remove : function() {
+            // ...
         }
-    }
+    };
 
     var Column = function () {
         this.cells = [];
@@ -42,20 +52,65 @@ var EdiTable = function (table, options) {
             if (typeof optValue == "undefined") optValue = "";
 
             // ...
+        },
+        remove : function() {
+            // ...
         }
     };
 
     var Cell = function () {
         this.td;
+        this.selected = false;
     };
     Cell.prototype = {
+        isEditable : function(){
+            return this.td.contenteditable;
+        },
         select : function(){
 
         }
     };
 
+    var Selection = function(rowFrom, rowTo, colFrom, colTo){
+        this.rowFrom = rowFrom;
+        this.rowTo = rowTo;
+        this.colFrom = colFrom;
+        this.colTo = colTo;
+    };
+    Selection.prototype = {
+
+    };
+
     this.rows = [];
     this.cols = [];
+
+
+    function copyTest(event) {
+        console.log(event, event.clipboardData);
+
+        event.clipboardData.setData("text/plain", "oiewfj");
+
+        event.preventDefault();
+    }
+    function cutTest(event){
+        // TODO
+    }
+    function pasteTest(event){
+        //Use innerText of tr's found in the template to paste stuff.
+
+        var docFrag = document.createElement("template");
+        var htmlText = event.clipboardData.getData("text/html");
+
+        console.log(htmlText);
+        docFrag.innerHTML = htmlText;
+        console.log(docFrag, docFrag.innerHTML);
+
+
+        event.preventDefault();
+    }
+    document.addEventListener("copy", copyTest);
+    document.addEventListener("cut", cutTest);
+    document.addEventListener("paste", pasteTest);
 };
 EdiTable.prototype = {
     select : function(/* parameters */){
