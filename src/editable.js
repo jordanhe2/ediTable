044a403f9fe848ebb91ff58892f68e9fa6a1e932
Jordan Hendrickson <jordanhe2@gmail.com>
2016-12-10 13:31:44 -0600

    Added support for Mac by replacing all uses of ctrl key with command key in keydown event.

--- src/editable.js
+++ src/editable.js
@@ -9,6 +9,81 @@
 (function () {
     "use strict";
 
+    /*  getOS function taken from https://github.com/thorst/supportedBrowser.js/blob/master/src/browserInfo.js
+        Licensed with MIT license Copyright (c) 2014 Todd Horst
+    */
+    var getOS = function () {
+        var nVer = navigator.appVersion;
+        var nAgt = navigator.userAgent;
+        var osVersion = "unknown";
+        var os = "unknown";
+        // system
+
+        var clientStrings = [
+            { s: 'Windows 3.11', r: /Win16/ },
+            { s: 'Windows 95', r: /(Windows 95|Win95|Windows_95)/ },
+            { s: 'Windows ME', r: /(Win 9x 4.90|Windows ME)/ },
+            { s: 'Windows 98', r: /(Windows 98|Win98)/ },
+            { s: 'Windows CE', r: /Windows CE/ },
+            { s: 'Windows 2000', r: /(Windows NT 5.0|Windows 2000)/ },
+            { s: 'Windows XP', r: /(Windows NT 5.1|Windows XP)/ },
+            { s: 'Windows Server 2003', r: /Windows NT 5.2/ },
+            { s: 'Windows Vista', r: /Windows NT 6.0/ },
+            { s: 'Windows 7', r: /(Windows 7|Windows NT 6.1)/ },
+            { s: 'Windows 8.1', r: /(Windows 8.1|Windows NT 6.3)/ },
+            { s: 'Windows 8', r: /(Windows 8|Windows NT 6.2)/ },
+            { s: 'Windows NT 4.0', r: /(Windows NT 4.0|WinNT4.0|WinNT|Windows NT)/ },
+            { s: 'Windows ME', r: /Windows ME/ },
+            { s: 'Android', r: /Android/ },
+            { s: 'Open BSD', r: /OpenBSD/ },
+            { s: 'Sun OS', r: /SunOS/ },
+            { s: 'Linux', r: /(Linux|X11)/ },
+            { s: 'iOS', r: /(iPhone|iPad|iPod)/ },
+            { s: 'Mac OS X', r: /Mac OS X/ },
+            { s: 'Mac OS', r: /(MacPPC|MacIntel|Mac_PowerPC|Macintosh)/ },
+            { s: 'QNX', r: /QNX/ },
+            { s: 'UNIX', r: /UNIX/ },
+            { s: 'BeOS', r: /BeOS/ },
+            { s: 'OS/2', r: /OS\/2/ },
+            { s: 'Search Bot', r: /(nuhk|Googlebot|Yammybot|Openbot|Slurp|MSNBot|Ask Jeeves\/Teoma|ia_archiver)/ }
+        ];
+        for (var id in clientStrings) {
+            var cs = clientStrings[id];
+            if (cs.r.test(nAgt)) {
+                os = cs.s;
+                break;
+            }
+        }
+
+
+
+        if (/Windows/.test(os)) {
+            osVersion = /Windows (.*)/.exec(os)[1];
+            os = 'Windows';
+        }
+
+        switch (os) {
+            case 'Mac OS X':
+                osVersion = /Mac OS X (10[\.\_\d]+)/.exec(nAgt)[1];
+                break;
+
+            case 'Android':
+                osVersion = /Android ([\.\_\d]+)/.exec(nAgt)[1];
+                break;
+
+            case 'iOS':
+                osVersion = /OS (\d+)_(\d+)_?(\d+)?/.exec(nVer);
+                osVersion = osVersion[1] + '.' + osVersion[2] + '.' + (osVersion[3] | 0);
+                break;
+
+        }
+
+        return {
+            name: os,
+            versionString: osVersion
+        };
+    };
+
     // FIX STUPID STUFF
     Number.prototype.mod = function (n) {
         return ((this % n) + n) % n;
@@ -517,6 +592,20 @@
                 table = $(that.table),
                 startCoords = [];
 
+            var controlDown = (function (os) {
+                var isMac = os.name.indexOf("Mac") > -1;
+
+                if (isMac) {
+                    return function (e) {
+                        return e.metaKey;
+                    }
+                } else {
+                    return function (e) {
+                        return e.ctrlKey;
+                    }
+                }
+            })(getOS());
+
             // Keeps track of double touch
             var lastTap;
             var ifDoubleTap = function () {
@@ -647,7 +736,7 @@
                 var hasSelection = that.hasSelection(),
                     singleCell = selection.originCell == selection.terminalCell,
                     code = e.keyCode,
-                    ctrl = e.ctrlKey,
+                    ctrl = controlDown(e),
                     shift = e.shiftKey,
                     tab = code == 9,
                     enter = code == 13,
